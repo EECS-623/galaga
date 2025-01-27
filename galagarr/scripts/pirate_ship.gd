bfb25cab11797b1bb054f0247dc036490128dbc2
andrewhuang2019 <andrewhuang2019@gmail.com>
2025-01-27 10:54:31 -0600

    Create Pirate Projectile
    Created Pirate projectile for the game

--- galagarr/scripts/pirate_ship.gd
+++ galagarr/scripts/pirate_ship.gd
@@ -3,6 +3,7 @@ signal hit
 var screen_size
 var time = 0
 @export var projectile: PackedScene
+@export var cannon_ball: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +14,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# Called when a cannon_ball hits the pirateship
 func _on_body_entered(body: Node2D) -> void:
-	hit.emit()
+	if body.name == cannon_ball.name:
+		print("Collided with: ", body.name)
+		queue_free()
 	
+
+func _shoot_projectile():
+	var new_projectile = projectile.instantiate()
+	
+	new_projectile.position = Vector2(0.0,0.0)
+	
+	owner.add_child(new_projectile)
 
 #func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	#queue_free()
