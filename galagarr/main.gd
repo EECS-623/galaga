38b40125dbb3e71a7a1c45cf4310731a49d29e6d
andrewhuang2019 <andrewhuang2019@gmail.com>
2025-01-26 22:03:33 -0600

    Updated Main scene
    Updated the menu button to become the "Main scene" so the game starts on the menu. it is a bit confusing.

--- galagarr/main.gd
+++ galagarr/main.gd
@@ -1,10 +1,16 @@
 extends Node
 
+@export var pirate_scene: PackedScene
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://menu.tscn")
+	#get_tree().change_scene_to_file("res://menu.tscn")
+	pass
 
+func new_game():
+	score = 0
+	$MobTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
--- galagarr/menu.gd
+++ galagarr/menu.gd
@@ -10,4 +10,4 @@ func _ready() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://player.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
