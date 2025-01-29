d825740c5a3f001991d750a00ab21042b8789e49
ChaseHorner <c710h797@ku.edu>
2025-01-28 23:49:25 -0600

    Rough controls page

--- galagarr/menu.gd
+++ galagarr/menu.gd
@@ -11,3 +11,7 @@ func _ready() -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://controls.tscn")
