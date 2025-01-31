3d1de6bf3d2e7817a43c9ff502620c4f9918fa1e
Andysword <andrewhuang2019@gmail.com>
2025-01-31 11:58:41 -0600

    Update game.gd
    Updated game with the music and pirates following paths

--- galagarr/scripts/game.gd
+++ galagarr/scripts/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @export var pirate_ship: PackedScene
+var num_enemies = 0
+var max_enemies = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,29 +15,29 @@ func _process(delta: float) -> void:
 # When the mob timer times out, spawn a child instance of pirate ship
 func _on_mob_timer_timeout() -> void:
 	
-	# while num enemies < max enemies:
-	var pirate = pirate_ship.instantiate()
-	
-	var path_follow = PathFollow2D.new()
-	
-	var pirate_ship_path = $PiratePath/BottomLeftPath
-	
-	pirate_ship_path.add_child(path_follow)
-	
-	path_follow.position = pirate_ship_path.get_position()
-	
-	path_follow.progress_ratio = 0
-	
-	#pirate ship rotate 
-	
-	print(path_follow.progress)
-	print(path_follow.position)
-	print(path_follow.global_position)
-	
-	#pirate = path_follow.position
-	
-	#pirate.global_position = 
-	#Vector2(randf_range(50.0, 1150.0), 120.0)
-	
-	path_follow.add_child(pirate)
-	
+	# if num enemies < max enemies (initially 10):
+	if (num_enemies < max_enemies): 
+		
+		# create new pirate and PathFollow2D
+		var pirate = pirate_ship.instantiate()
+		var path_follow = PathFollow2D.new()
+		
+		# find path for pirate to spawn out of 
+		var rng = RandomNumberGenerator.new()
+		var random_number = rng.randi_range(0, 1)
+		var pirate_ship_path
+		if (random_number == 0):
+			pirate_ship_path = $PiratePath/BottomLeftPath
+		elif (random_number == 1):
+			pirate_ship_path = $PiratePath/BottomRightPath
+		
+		pirate_ship_path.add_child(path_follow)
+		
+		path_follow.position = pirate_ship_path.get_position()
+		
+		path_follow.progress_ratio = 0
+		
+		pirate.rotation -= PI / 2
+		
+		path_follow.add_child(pirate)
+		num_enemies += 1
