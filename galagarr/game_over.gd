5a537bf72934c65cf3f41f5027f619d2274a6992
Joshua Lee <joshua.alee888@gmail.com>
2025-02-01 18:49:30 -0600

    play again bug fix

--- galagarr/game_over.gd
+++ galagarr/game_over.gd
@@ -2,11 +2,11 @@ extends Control
 
 func _ready() -> void:
 	# Set focus on the Play Again button so the user can quickly restart
-	$VBoxContainer/PlayAgain.grab_focus()
+	$VBoxContainer2/PlayAgain.grab_focus()
 
 func _on_play_again_pressed() -> void:
 	# Change back to the main game scene
-	get_tree().change_scene("res://main.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_quit_pressed() -> void:
 	# Quit the game
